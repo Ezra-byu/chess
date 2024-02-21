145b805008011e0147792f535d031ad3d488bb62
Ezra Bradley <ezrabradley@Ezras-MacBook-Pro-2.local>
2024-02-20 22:39:36 -0700

    Phase 3 inititalization

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,5 +1,5 @@
 import chess.*;
-
+//Phase 3
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
