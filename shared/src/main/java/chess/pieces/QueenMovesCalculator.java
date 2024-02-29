9dcfd6b4f5cf7f7d2ab028c507e0d4640a07dbb4
Ezra Bradley <ezrabradley@Ezras-MBP-2.app.byu.edu>
2024-02-29 12:53:25 -0700

    Style edits to ResponseException

--- shared/src/main/java/chess/pieces/QueenMovesCalculator.java
+++ shared/src/main/java/chess/pieces/QueenMovesCalculator.java
@@ -26,10 +26,10 @@ public class QueenMovesCalculator implements PieceMovesCalculator {
         diagonalChecker(-1, 1, board, position);
         diagonalChecker(-1, -1, board, position);
 
-        orthogonalChecker(0, 1, board, position);
-        orthogonalChecker(0, -1, board, position);
-        orthogonalChecker(1, 0, board, position);
-        orthogonalChecker(-1, 0, board, position);
+        diagonalChecker(0, 1, board, position);
+        diagonalChecker(0, -1, board, position);
+        diagonalChecker(1, 0, board, position);
+        diagonalChecker(-1, 0, board, position);
 
         return moves_Set;
     }
@@ -40,9 +40,6 @@ public class QueenMovesCalculator implements PieceMovesCalculator {
         row += rowincr;
         col += colincr;
         while((row >= 1) && (row <= 8) && (col >= 1) && (col <= 8)){
-            //System.out.println("deeper in loop");
-            //System.out.println(row);
-            //System.out.println(col);
             tempposition = new ChessPosition(row , col); //create the position being investigated
             if (board.getPiece(tempposition) != null) { //if there is a piece in the position being investigated
                 if (board.getPiece(tempposition).getTeamColor() != piece.getTeamColor()){ //if piece not same team
@@ -61,37 +58,6 @@ public class QueenMovesCalculator implements PieceMovesCalculator {
             }
             row += rowincr;
             col += colincr;
-        }
-    }
-    private void orthogonalChecker(int rowincr, int colincr, ChessBoard board, ChessPosition position) {
-//        System.out.println("in loop");
-        row = position.getRow() ;
-        col = position.getColumn();
-        row += rowincr;
-        col += colincr;
-
-        while((row >= 1) && (row <= 8) && (col >= 1) && (col <= 8)){
-            System.out.println("deeper in loop");
-            System.out.println(row);
-            System.out.println(col);
-            tempposition = new ChessPosition(row , col); //create the position being investigated
-            if (board.getPiece(tempposition) != null) { //if there is a piece in the position being investigated
-                if (board.getPiece(tempposition).getTeamColor() != piece.getTeamColor()){ //if piece not same team
-                    //add tempposition to the moves_set
-                    moves_Set.add(new ChessMove(position, tempposition, null));
-                    break;
-                }
-                else if (board.getPiece(tempposition).getTeamColor() == piece.getTeamColor()){
-                    break;
-                }
-                else {throw new RuntimeException("Team Color equal method error");}
-            }
-            else{ // if no piece in the position of interest
-                //add tempposition to the move array
-                moves_Set.add(new ChessMove(position, tempposition, null));
-            }
-            row += rowincr; //increment row
-            col += colincr; //incrament col
         }
     }
 }
