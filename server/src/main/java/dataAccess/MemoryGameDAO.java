b1d6fb01bb71c5ae17d1d35a49d243fdedc92159
Ezra Bradley <ezrabradley@Ezras-MBP-2.app.byu.edu>
2024-02-28 17:44:51 -0700

    passing all CreateGame tests as it now passes Valid Creation

--- server/src/main/java/dataAccess/MemoryGameDAO.java
+++ server/src/main/java/dataAccess/MemoryGameDAO.java
@@ -1,16 +1,20 @@
 package dataAccess;
 
+import chess.ChessGame;
 import model.GameData;
 
 import java.util.Collection;
 import java.util.HashMap;
 
 public class MemoryGameDAO implements GameDAO{
+    private int nextId = 1;
     final private HashMap<Integer, GameData> games = new HashMap<>();//{gameID, GameData}
     @Override
     public GameData createGame(GameData game) {
-        games.put(game.gameID(), game);
-        return game;
+        //int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game
+        GameData newgame = new GameData(nextId++, game.whiteUsername(), game.blackUsername(), game.gameName(), game.game());
+        games.put(newgame.gameID(), newgame);
+        return newgame;
     }
 
     @Override
