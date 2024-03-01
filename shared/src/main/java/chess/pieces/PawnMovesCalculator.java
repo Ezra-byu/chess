7975f35da7e8dd14b4b248ed0961a850c868e4cc
Ezra Bradley <ezrabradley@Ezras-MacBook-Pro-2.local>
2024-02-29 20:42:06 -0700

    Style edits

--- shared/src/main/java/chess/pieces/PawnMovesCalculator.java
+++ shared/src/main/java/chess/pieces/PawnMovesCalculator.java
@@ -26,16 +26,8 @@ public class PawnMovesCalculator implements PieceMovesCalculator {
             //try move +1
             proposedposition = new ChessPosition(position.getRow()+1 , position.getColumn());
             if (isInBounds(proposedposition) && !isBlocked(proposedposition)){ //if it can move forward
-                if (whitePromotion(proposedposition)) { //single move promotion
-                    movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.QUEEN));
-                    movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.BISHOP));
-                    movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.ROOK));
-                    movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.KNIGHT));
-                }
-                else{ //single move no promotion white
-                    movesSet.add(new ChessMove(position, proposedposition, null));
-                }
-
+                //single move promotion
+                this.whitePawnSinglePromotion(proposedposition);
                 // check for the double move white
                 proposedposition = new ChessPosition(position.getRow()+2 , position.getColumn());
                 if (isInBounds(proposedposition) && !isBlocked(proposedposition) && whiteDoubleStart(position)){
@@ -43,44 +35,25 @@ public class PawnMovesCalculator implements PieceMovesCalculator {
                 }
             }
             //try right diagonal
-            ChessPosition proposedposition1 = new ChessPosition(position.getRow()+1 , position.getColumn()+1);
-            if (isInBounds(proposedposition1) && !diagIsBlocked(proposedposition)) {
-                if (whitePromotion(proposedposition1)) { //single move promotion
-                    movesSet.add(new ChessMove(position, proposedposition1, ChessPiece.PieceType.QUEEN));
-                    movesSet.add(new ChessMove(position, proposedposition1, ChessPiece.PieceType.BISHOP));
-                    movesSet.add(new ChessMove(position, proposedposition1, ChessPiece.PieceType.ROOK));
-                    movesSet.add(new ChessMove(position, proposedposition1, ChessPiece.PieceType.KNIGHT));
-                } else { //right diag move no promotion white
-                    movesSet.add(new ChessMove(position, proposedposition1, null));
-                }
+            ChessPosition proposedposition = new ChessPosition(position.getRow()+1 , position.getColumn()+1);
+            if (isInBounds(proposedposition) && !diagIsBlocked(proposedposition)) {
+                //single move promotion
+                this.whitePawnSinglePromotion(proposedposition);
             }
 
             //try right diagonal
             proposedposition = new ChessPosition(position.getRow()+1 , position.getColumn()-1);
             if (isInBounds(proposedposition) && !diagIsBlocked(proposedposition)) {
-                if (whitePromotion(proposedposition)) { //single move promotion
-                    movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.QUEEN));
-                    movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.BISHOP));
-                    movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.ROOK));
-                    movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.KNIGHT));
-                } else { //left diag move no promotion white
-                    movesSet.add(new ChessMove(position, proposedposition, null));
-                }
+                //single move promotion
+                this.whitePawnSinglePromotion(proposedposition);
             }
         }
         else if (board.getPiece(position).getTeamColor() == ChessGame.TeamColor.BLACK){ // if piece Black
             //try move -1
             proposedposition = new ChessPosition(position.getRow()-1 , position.getColumn());
             if (isInBounds(proposedposition) && !isBlocked(proposedposition)){//if it can move forward
-                if (blackPromotion(proposedposition)) { //single move promotion
-                    movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.QUEEN));
-                    movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.BISHOP));
-                    movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.ROOK));
-                    movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.KNIGHT));
-                }
-                else{ //single move no promotion
-                    movesSet.add(new ChessMove(position, proposedposition, null));
-                }
+                //single move promotion
+                this.blackPawnSinglePromotion(proposedposition);
                 // check for the double move
                 proposedposition = new ChessPosition(position.getRow()-2 , position.getColumn());
                 if (isInBounds(proposedposition) && !isBlocked(proposedposition) && blackDoubleStart(position)){
@@ -91,36 +64,45 @@ public class PawnMovesCalculator implements PieceMovesCalculator {
             //try right diagonal black
             proposedposition = new ChessPosition(position.getRow()-1 , position.getColumn()+1);
             if (isInBounds(proposedposition) && !diagIsBlocked(proposedposition)) {
-                if (blackPromotion(proposedposition)) { //single move promotion
-                    movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.QUEEN));
-                    movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.BISHOP));
-                    movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.ROOK));
-                    movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.KNIGHT));
-                }
-                else { //right diag move no promotion black
-                    movesSet.add(new ChessMove(position, proposedposition, null));
-                }
+                //single move promotion
+                this.blackPawnSinglePromotion(proposedposition);
             }
 
             //try left diagonal black
             proposedposition = new ChessPosition(position.getRow()-1 , position.getColumn()-1);
             //System.out.print(diagIsBlocked(proposedposition));
             if (isInBounds(proposedposition) && !diagIsBlocked(proposedposition)) {
-                if (blackPromotion(proposedposition)) {
-                    movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.QUEEN));
-                    movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.BISHOP));
-                    movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.ROOK));
-                    movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.KNIGHT));
-                }
-                else { //left diag move no promotion black
-                    movesSet.add(new ChessMove(position, proposedposition, null));
-                }
+                //single move promotion
+                this.blackPawnSinglePromotion(proposedposition);
             }
 
         }
         return movesSet;
     }
 
+    private void whitePawnSinglePromotion(ChessPosition position){
+        if (whitePromotion(proposedposition)) { //single move promotion
+            movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.QUEEN));
+            movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.BISHOP));
+            movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.ROOK));
+            movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.KNIGHT));
+        }
+        else{ //single move no promotion white
+            movesSet.add(new ChessMove(position, proposedposition, null));
+        }
+    }
+
+    private void blackPawnSinglePromotion(ChessPosition position){
+        if (blackPromotion(proposedposition)) { //single move promotion
+            movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.QUEEN));
+            movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.BISHOP));
+            movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.ROOK));
+            movesSet.add(new ChessMove(position, proposedposition, ChessPiece.PieceType.KNIGHT));
+        }
+        else{ //single move no promotion
+            movesSet.add(new ChessMove(position, proposedposition, null));
+        }
+    }
     private Boolean isInBounds(ChessPosition position){
         if ((position.getRow() >= 1) && (position.getRow() <= 8) && (position.getColumn() >= 1) && (position.getColumn() <= 8)){
             return Boolean.TRUE;
