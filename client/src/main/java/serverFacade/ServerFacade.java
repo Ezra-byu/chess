98eead543cd65bc2915c0019e7c57c5deb31b928
Ezra Bradley <ezrabradley@Ezras-MBP-2.app.byu.edu>
2024-03-20 19:29:41 -0600

    ServerFacade Tests complete

---
+++ client/src/main/java/serverFacade/ServerFacade.java
@@ -0,0 +1,141 @@
+package serverFacade;
+
+import com.google.gson.Gson;
+import exception.ResponseException;
+import model.AuthData;
+import model.GameData;
+import model.JoinGameRequest;
+import model.UserData;
+import model.responses.ListGameResponse;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.util.Collection;
+
+public class ServerFacade {
+
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+
+    public AuthData register(UserData user) throws ResponseException {
+        var path = "/user";
+        return this.makeRequest("POST", path, user, AuthData.class, null);
+    }
+
+    public AuthData login(UserData user) throws ResponseException {
+        var path = "/session";
+        return this.makeRequest("POST", path, user, AuthData.class, null);
+    }
+
+    public void logout(String authToken) throws ResponseException {
+        var path = "/session";
+        this.makeRequest("DELETE", path, null, null, authToken);
+//        try {
+//            var path = "/user";
+//            this.makeRequest("DELETE", path, user, null);
+//        } catch (Exception e) {
+//            System.out.println("500" + "Something went wrong." + e);
+//        }
+    }
+
+    public GameData[] listGames(String authToken) throws ResponseException {
+        var path = "/game";
+        Collection<GameData> gameList;
+        var response = this.makeRequest("GET", path, null, ListGameResponse.class, authToken);
+        return response.getGames().toArray(new GameData[0]);
+//        try {
+//            var path = "/game";
+//            record listPetResponse(GameData[] gameRecord) {
+//            }
+//            var response = this.makeRequest("GET", path, null, listPetResponse.class);
+//            return response.gameRecord();
+//        } catch (Exception e) {
+//            System.out.println("500" + "Something went wrong." + e);
+//        }
+//        return null;
+    }
+    public GameData createGame(GameData game, String authToken) throws ResponseException{
+        var path = "/game";
+        return this.makeRequest("POST", path, game, GameData.class,authToken);
+//        try {
+//            var path = "/game";
+//            return this.makeRequest("POST", path, game, GameData.class);
+//        } catch (Exception e) {
+//            System.out.println("500" + "Something went wrong." + e);
+//        }
+//        return null;
+    }
+
+    public void joinGame(JoinGameRequest gameRequest, String authToken) throws ResponseException{
+        var path = "/game";
+        this.makeRequest("PUT", path, gameRequest, GameData.class, authToken);
+//        try {
+//            var path = "/game";
+//            this.makeRequest("PUT", path, gameRequest, GameData.class);
+//        } catch (Exception e) {
+//            System.out.println("500" + "Something went wrong." + e);
+//        }
+    }
+
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass, String authToken) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setRequestProperty("authorization", authToken);
+            http.setDoOutput(true);
+            writeBody(request, http);//serialization and sends
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            throw new ResponseException(status, "failure: " + status);
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+}
--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -1,18 +1,38 @@
 package clientTests;
 
+import chess.ChessGame;
+import exception.ResponseException;
+import model.GameData;
+import model.JoinGameRequest;
+import model.UserData;
+import model.responses.ListGameResponse;
 import org.junit.jupiter.api.*;
 import server.Server;
+import serverFacade.ServerFacade;
+import service.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade serverFacade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        var url = "http://localhost:" + port;
+        serverFacade = new ServerFacade(url);
+    }
+    @BeforeEach
+    void clear() throws ResponseException {
+        Service.clear();
     }
 
     @AfterAll
@@ -22,8 +42,134 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void registerSuccess() throws ResponseException {
+        var createdUser = new UserData("john", "jacob", "jingheimer@gmail");
+        var returnedAuth = assertDoesNotThrow(() -> serverFacade.register(createdUser));
+        Assertions.assertNotNull(returnedAuth);
+        Assertions.assertNotNull(returnedAuth.authToken());
+    }
+
+    @Test
+    void registerFailure() {
+        var createdUser = new UserData("john", "jacob", "jingheimer@gmail");
+        var returnedAuth = assertDoesNotThrow(() -> serverFacade.register(createdUser));
+        Assertions.assertNotNull(returnedAuth);
+        Assertions.assertNotNull(returnedAuth.authToken());
+
+        var response = assertThrows(ResponseException.class, () -> serverFacade.register(createdUser));
+
+
+    }
+
+    @Test
+    void loginSuccess() {
+        var createdUser = new UserData("john", "jacob", "jingheimer@gmail");
+        //try login without register
+        var response = assertThrows(ResponseException.class, () -> serverFacade.login(createdUser));
+
+        var returnedAuth = assertDoesNotThrow(() -> serverFacade.register(createdUser));
+        returnedAuth = assertDoesNotThrow(() -> serverFacade.login(createdUser));
+        Assertions.assertNotNull(returnedAuth);
+        Assertions.assertNotNull(returnedAuth.authToken());
+
+    }
+
+    @Test
+    void loginFailure() {
+        var createdUser = new UserData("john", "jacob", null);
+        //try login without register
+        var response = assertThrows(ResponseException.class, () -> serverFacade.login(createdUser));
+    }
+
+    @Test
+    void logoutSuccess() throws ResponseException {
+        var createdUser = new UserData("john", "jacob", "jingheimer@gmail");
+        var returnedAuth = assertDoesNotThrow(() -> serverFacade.register(createdUser));
+
+        Assertions.assertNotNull(returnedAuth);
+        Assertions.assertNotNull(returnedAuth.authToken());
+
+        assertDoesNotThrow(() -> serverFacade.logout(returnedAuth.authToken()));
+    }
+
+    @Test
+    void logoutFailure() throws ResponseException {
+        var createdUser = new UserData("john", "jacob", null);
+        //try logout without login
+        var response = assertThrows(ResponseException.class, () -> serverFacade.login(createdUser));
+    }
+
+    @Test
+    void listGamesSuccess() throws ResponseException {
+        var createdUser = new UserData("john", "jacob", "jingheimer@gmail");
+        var returnedAuth = assertDoesNotThrow(() -> serverFacade.register(createdUser));
+
+        var gameList = assertDoesNotThrow(() -> serverFacade.listGames(returnedAuth.authToken()));
+        assertEquals(0, gameList.length);
+
+        GameData createdGame = new GameData(0, null, null, "mygame", null);
+        var returnedGame = serverFacade.createGame(createdGame, returnedAuth.authToken());
+        var createdGameRequest = new JoinGameRequest("WHITE", returnedGame.gameID());
+        assertDoesNotThrow(() -> serverFacade.joinGame(createdGameRequest, returnedAuth.authToken()));
+    }
+    @Test
+    void listGamesFailure() throws ResponseException {
+        var createdUser = new UserData("john", "jacob", "jingheimer@gmail");
+        var returnedAuth = assertDoesNotThrow(() -> serverFacade.register(createdUser));
+
+        GameData createdGame1 = new GameData(0, null, null, "mygame1", null);
+        GameData createdGame2 = new GameData(0, null, null, "mygame2", null);
+        var returnedGame1 = serverFacade.createGame(createdGame1, returnedAuth.authToken());
+        var returnedGame2 = serverFacade.createGame(createdGame2, returnedAuth.authToken());
+        var gameList = assertDoesNotThrow(() -> serverFacade.listGames(returnedAuth.authToken()));
+        assertEquals(2, gameList.length);
+        assertNotEquals(1, gameList.length);
+    }
+
+    @Test
+    void createGameSuccess() throws ResponseException {
+        var createdUser = new UserData("john", "jacob", "jingheimer@gmail");
+        var returnedAuth = assertDoesNotThrow(() -> serverFacade.register(createdUser));
+
+        GameData createdGame1 = new GameData(0, null, null, "mygame1", null);
+        GameData createdGame2 = new GameData(0, null, null, "mygame2", null);
+
+        var returned = serverFacade.createGame(createdGame1, returnedAuth.authToken());
+        assertEquals(1, returned.gameID());
+
+        returned = serverFacade.createGame(createdGame2, returnedAuth.authToken());
+        assertEquals(2, returned.gameID());
+
+    }
+
+    @Test
+    void createGameFailure() throws ResponseException {
+        var createdUser = new UserData("john", "jacob", "jingheimer@gmail");
+        var returnedAuth = assertDoesNotThrow(() -> serverFacade.register(createdUser));
+
+
+        GameData createdGame1 = new GameData(0, null, null, null, null);
+        var badResponse = assertThrows(ResponseException.class, () -> serverFacade.createGame(createdGame1, returnedAuth.authToken()));
+
+        GameData createdGame2 = new GameData(0, null, null, "mygame", null);
+        var goodResponse = assertDoesNotThrow(() -> serverFacade.createGame(createdGame2, returnedAuth.authToken()));
+    }
+
+    @Test
+    void joinGameSuccess() throws ResponseException {
+        var createdUser = new UserData("john", "jacob", "jingheimer@gmail");
+        var returnedAuth = assertDoesNotThrow(() -> serverFacade.register(createdUser));
+
+        GameData createdGame1 = new GameData(0, null, null, "mygame1", null);
+        var returnedGame = serverFacade.createGame(createdGame1, returnedAuth.authToken());
+        var createdGameRequest = new JoinGameRequest("WHITE", returnedGame.gameID());
+        assertDoesNotThrow(() -> serverFacade.joinGame(createdGameRequest, returnedAuth.authToken()));
+    }
+
+    @Test
+    void joinGameFailure() throws ResponseException {
+        var createdUser = new UserData("john", "jacob", "jingheimer@gmail");
+        var returnedAuth = assertDoesNotThrow(() -> serverFacade.register(createdUser));
     }
 
 }
